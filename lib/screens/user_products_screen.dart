cd7e20a2105a5125bf49be36e12a1879f216f8fd
AniketDubey <aniket.dubey0206@gmail.com>
2021-05-03 14:08:39 +0530

    added features of edit and delete products for users

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
-import '../screens/edit_product_screen.dart';
-import '../widgets/user_product_item.dart';
+
 import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
-  static const routeName = "/user-products";
+  static const routeName = '/user-products';
 
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<Products>(context);
+    final productsData = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Your Products"),
+        title: const Text('Your Products'),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
@@ -28,10 +28,16 @@ class UserProductsScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemCount: productData.items.length,
-          itemBuilder: (_, i) => UserProductItem(
-            productData.items[i].title,
-            productData.items[i].imageUrl,
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].id,
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
           ),
         ),
       ),
